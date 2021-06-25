1b7c00cf6220c314b87991fd277ac618d88810e7
Can ŞANLI <cansanli.of@gmail.com>
2021-06-25 13:47:25 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+
+
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+
+        public void Add(Car car)
+        {
+            //İş komutları(koşullar vb.)
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            //İş komutları(koşullar vb.)
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            //İş komutları(koşullar vb.)
+            return _carDal.GetAll();
+        }
+
+        public Car GetById(int Id)
+        {
+            //İş komutları(koşullar vb.)
+            return _carDal.GetById(Id);
+        }
+
+        public void Update(Car car)
+        {
+            //İş komutları(koşullar vb.)
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //++++++++++++++++++++++++++++++++++++++++++++++
+
+            //Console.WriteLine(carManager.GetById(1).Description);
+
+            //++++++++++++++++++++++++++++++++++++++++++++++
+
+            carManager.Add(new Car { Id = 2, ColorId = 1, BrandId = 2, Description = "Mercedes", DailyPrice = 350, ModelYear = 2018 });
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //++++++++++++++++++++++++++++++++++++++++++++++
+
+            carManager.Update(new Car { Id = 1, BrandId = 2, ColorId = 3, DailyPrice = 500, ModelYear = 2020, Description = "Tofaş" });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            //++++++++++++++++++++++++++++++++++++++++++++++
+
+            carManager.Delete(new Car { Id = 1 });
+            Console.WriteLine();
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+        }
+    }
+}
